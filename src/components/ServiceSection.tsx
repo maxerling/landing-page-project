522b537b0d2d67da855a184c9b95d68a1c54358b
maxerling <maximilianerling@gmail.com>
2021-06-21 11:43:04 +0200

    started on servicesection

--- src/components/ServiceSection.tsx
+++ src/components/ServiceSection.tsx
@@ -3,12 +3,28 @@ import styled from "styled-components";
 
 interface ServiceSectionProps {}
 
-const HeaderText = styled.h1`
-display: flex;
-width: 100%;
+const ContnetWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #eec800;
+`;
 
+
+
+const Text = styled.h2``;
+
+const HeaderText = styled.h1`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  width: 100%;
+  justify-content: center;
 `;
 
 export const ServiceSection: React.FC<ServiceSectionProps> = ({}) => {
-  return <></>;
+  return (
+    <ContnetWrapper>
+      <HeaderText>Tjänster</HeaderText>
+    </ContnetWrapper>
+  );
 };
