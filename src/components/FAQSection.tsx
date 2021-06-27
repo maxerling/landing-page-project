ac0b67e28f1d0aee72fbe85899c5b62060c01cf7
maxerling <maximilianerling@gmail.com>
2021-06-27 12:41:16 +0200

    setup FAQSection

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 interface FAQSectionProps {}
@@ -20,11 +20,75 @@ const HeaderText = styled.h1`
   font-weight: 600;
 `;
 
+const FAQ = styled.div`
+  border: 2px solid;
+  justify-content: center;
+  flex-direction: wrap;
+  width: 100%;
+  padding: 1em;
+  margin: 1em;
+`;
+const FAQQ = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  font-size: 1.563rem;
+  font-weight: 600;
+
+  &:active {
+    content: "";
+  }
+`;
+
+const FAQA = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  opacity: 0;
+  max-height: 0;
+  overflow: hidden;
+  transition: all 0.4s ease;
+`;
+
 export const FAQSection: React.FC<FAQSectionProps> = ({}) => {
+  const questions = [
+    {
+      question: "Hur kontaktar jag er?",
+      answer:
+        "Via vårt kontaktformulär alternativt via telefon på detta nummer: 07xxxxxxxx.",
+      open: false,
+    },
+    {
+      question: "När kan ni jobba?",
+      answer: "Kontakta oss och sedan återkommer vi med ett svar.",
+      open: false,
+    },
+  ];
+
+  const [faqs, setfaqs] = useState(questions);
+  const toogleFAQ = (index: number) => {
+    setfaqs(
+      faqs.map((faq, i) => {
+        if (i === index) {
+          console.log(faq);
+          faq.open = !faq.open;
+        } else {
+          faq.open = false;
+        }
+
+        return faq;
+      })
+    );
+  };
   return (
     <ContentWrapper>
       <HeaderText>FAQ</HeaderText>
-      
+      {faqs.map((faq: any, i: number) => (
+        <FAQ className={faq.open ? "open" : ""} onClick={() => toogleFAQ(i)}>
+          <FAQQ>{faq.question}</FAQQ>
+          <FAQA>{faq.answer}</FAQA>
+        </FAQ>
+      ))}
     </ContentWrapper>
   );
 };
